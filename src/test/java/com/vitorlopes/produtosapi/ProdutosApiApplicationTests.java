f9ba9862ac729bb287debaffc91f82f7ef0a5cc6
Vitor Lopes <vgabriellopes1@gmail.com>
2025-01-29 12:14:49 -0300

    Initial commit

---
+++ src/test/java/com/vitorlopes/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorlopes.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
